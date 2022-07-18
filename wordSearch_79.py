4ca89f10943cdcd7f722487c6cc5aaaa5c76a6dc
96-malay <mshah95@stevens.edu>
2022-07-18 16:26:40 -0400

    Leetcode 79

---
+++ wordSearch_79.py
@@ -0,0 +1,36 @@
+def exist(board, word):
+    visit = set()
+
+    def check(i, j, counter):
+        if counter == len(word) - 1 and board[i][j] == word[counter]:
+            return True
+        elif counter == len(word) - 1 and board[i][j] != word[counter]:
+            return False
+        counter += 1
+        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+        for r, c in directions:
+            row, col = i+r, j+c
+            if (row in range(len(board)) and
+                col in range(len(board[row])) and
+                counter <= len(word) - 1 and
+                    board[row][col] == word[counter] and
+                    (row, col) not in visit):
+                print(board[row][col])
+                visit.add((row, col))
+                if check(row, col, counter):
+                    return True
+
+    for i in range(len(board)):
+        for j in range(len(board[i])):
+            visit.clear()
+            if board[i][j] == word[0]:
+                visit.add((i, j))
+                if check(i, j, 0):
+
+                    return True
+    return False
+
+
+# Bug
+print(exist([["A", "B", "C", "E"], ["S", "F", "E", "S"], ["A", "D", "E", "E"]],
+            "ABCESEEEFS"))
