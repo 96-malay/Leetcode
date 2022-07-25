c0c6b50f707bf307f5a29e21cc339d01977cc806
96-malay <mshah95@stevens.edu>
2022-07-25 17:19:20 -0400

    Leetcode 56

---
+++ mergeIntervals_56.py
@@ -0,0 +1,13 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+
+        intervals.sort(key=lambda x: x[0])
+        ans = []
+        for i in intervals:
+            if not ans:
+                ans.append(i)
+            if ans[-1][1] >= i[0]:
+                ans[-1][1] = max(i[1], ans[-1][1])
+            else:
+                ans.append(i)
+        return ans
